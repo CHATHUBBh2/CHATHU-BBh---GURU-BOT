7b5795e73bc0d180100bea1e65fee07d12b93582
CHATHUBBh2 <137495447+CHATHUBBh2@users.noreply.github.com>
2023-06-23 15:37:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94711714067', 'Owner', true],
+  ['94711714067', 'CHATHU BBh', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'CHATHU_BBH'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
